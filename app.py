9206460f1394f97cd8addb4e08c944ea8f4edd13
lonneke-dev <lonneke1908@outlook.com>
2021-07-28 19:32:26 +0000

    Added Sign In template and functionality

--- app.py
+++ app.py
@@ -47,6 +47,28 @@ def signup():
     return render_template("signup.html")
 
 
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("signin"))
+
+        else:
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("signin"))
+
+    return render_template("signin.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
