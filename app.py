174eaf647885ae6493a2fb046fbc524b0b395b8e
lonneke-dev <lonneke1908@outlook.com>
2021-08-03 14:19:17 +0000

    Added an edit button

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-
 mongo = PyMongo(app)
 
 
