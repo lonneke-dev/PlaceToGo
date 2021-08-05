0b288ce8c2d4cb1714c66277293d8fce9e0deda4
lonneke-dev <lonneke1908@outlook.com>
2021-08-05 10:38:44 +0000

    Added a functional 404 template

--- app.py
+++ app.py
@@ -144,6 +144,11 @@ def delete_place(place_id):
     return redirect(url_for("get_places"))
 
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
