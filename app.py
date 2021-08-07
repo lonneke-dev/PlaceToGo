68d25555ba00423aec45af87657be43d677d8d59
lonneke-dev <lonneke1908@outlook.com>
2021-08-07 09:41:34 +0000

    Set debug to False

--- app.py
+++ app.py
@@ -225,4 +225,4 @@ def not_found(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
